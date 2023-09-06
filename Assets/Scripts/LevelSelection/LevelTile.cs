87a49fda7601b6452ce2ec91cbf93208fcdb5cb8
JornKrediet <JKredietW@gmail.com>
2023-09-06 23:01:39 +0200

    Begin tiles

--- Assets/Scripts/LevelSelection/LevelTile.cs
+++ Assets/Scripts/LevelSelection/LevelTile.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
 
 namespace JK.Roguelike
@@ -7,14 +7,21 @@ namespace JK.Roguelike
     public class LevelTile : MonoBehaviour
     {
         private bool isOpenRoom = true;
-
+        private TileType type;
+        public void Initialize(bool isLast)
+        {
+            if (isLast)
+                type = TileType.EndBoss;
+            else
+                type = (TileType)UnityEngine.Random.Range(0, Enum.GetValues(typeof(TileType)).Cast<float>().Max() - 1);
+        }
     }
 
-    public enum TileTypes
+    public enum TileType
     {
-        Default,
+        Combat,
         Treasure,
-        Boss,
+        MiniBoss,
         EndBoss,
     }
 }
